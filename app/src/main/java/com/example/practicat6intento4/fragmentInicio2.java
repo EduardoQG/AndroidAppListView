9174c981a8f32aa668d63f6cc38793f05ddc97c5
EduardoQG <eduquiroga8@gmail.com>
2024-01-30 08:55:39 +0100

    Añadido menú inicial. App finalizada.

--- app/src/main/java/com/example/practicat6intento4/fragmentInicio2.java
+++ app/src/main/java/com/example/practicat6intento4/fragmentInicio2.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link inicioFragment#newInstance} factory method to
+ * Use the {@link fragmentInicio2#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class inicioFragment extends Fragment {
+public class fragmentInicio2 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class inicioFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public inicioFragment() {
+    public fragmentInicio2() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class inicioFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment inicioFragment.
+     * @return A new instance of fragment fragmentInicio2.
      */
     // TODO: Rename and change types and number of parameters
-    public static inicioFragment newInstance(String param1, String param2) {
-        inicioFragment fragment = new inicioFragment();
+    public static fragmentInicio2 newInstance(String param1, String param2) {
+        fragmentInicio2 fragment = new fragmentInicio2();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class inicioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        return inflater.inflate(R.layout.fragment_inicio2, container, false);
     }
 }
